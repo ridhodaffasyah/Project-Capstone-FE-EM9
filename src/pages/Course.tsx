270d8ad171cb587e1fb0c16ca07743df4f8367bf
raihangh <raihanghassani7@gmail.com>
2022-07-04 15:49:48 +0700

    membuat tabel list course, menambahkan deskripsi, mengubah route

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
+import { useParams } from 'react-router-dom';
 import {
   Button,
   Container
 } from '@chakra-ui/react';
 import data from "../Data";
 
+
 const Course = () => {
+  let { title} = useParams<{title?: string | undefined}>();
+  console.log(title)
+  const find = data.filter(element => element.title === title).map(filteredTitle => {
+    return (
+      <div className="container">
+        <div className="center">
+          <h1 className="text1">{filteredTitle.title}</h1>
+        </div>
+        <iframe width="855" height="481" src={filteredTitle.url} title={filteredTitle.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <h2 className="text1">Details</h2>
+        <p>{filteredTitle.details}</p>
+      </div>
+    )
+  });
+
   return (
     <div>
       <header>
@@ -14,25 +31,11 @@ const Course = () => {
           <Navbar />
         </div>
       </header>
-      <div className="container">
-        <div className="center">
-            <h1 className="text1">{data[0].title}</h1>
-        </div>
-            <iframe width="855" height="481" src={data[0].url} title={data[0].title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            <h2 className="text1">Details</h2>
-            <p>{data[0].details}</p>
-      {/* {data.map((d) => {
-          return (
-            <div>
-                <iframe width="855" height="481" src={d.url} title={d.title}></iframe>
-                <h1>{d.title}</h1>
-                <p>{d.details}</p>
-            </div>
-          );
-        })} */}
-      </div>
+      {find}
     </div>
-  );
-};
+  )
+}
 
 export default Course;
+
+
--- src/pages/CourseList.tsx
+++ src/pages/CourseList.tsx
@@ -1,5 +1,70 @@
 import React from 'react';
+import Navbar from "../components/Navbar";
+import { Link } from 'react-router-dom';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Stack,
+  Image,
+  Text,
+} from '@chakra-ui/react'
+import data from "../Data";
 
-export const CourseList = () => {
-  return <div>CourseList</div>;
+
+const CourseList = () => {
+  const dataTable = data.map((item) => {
+    return (
+      <Tr>
+        <Td>
+          <Stack direction='row'>
+            <Image
+              boxSize='50px'
+              objectFit='cover'
+              src='/assets/images/img3.png'
+            />
+            <Text fontSize='md' display="flex" alignItems="center">{item.title}</Text>
+          </Stack>
+        </Td>
+        <Td>{item.describe}</Td>
+        <Td>20 februari</Td>
+        <Th>
+          <Link to={`/course/${item.title}`}>Masuk</Link>
+        </Th>
+      </Tr>
+    );
+  });
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <Navbar />
+        </div>
+      </header>
+      <div className="containerCourse-list">
+        <h1>All Courses</h1>
+        <TableContainer w='70%'>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>Name Course</Th>
+                <Th>Description</Th>
+                <Th>Deadline</Th>
+                <Th>Progress</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {dataTable}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 };
+
+export default CourseList;
